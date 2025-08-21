4d218c07792923d6f76b729fd30b9a02bcd58980
syedhaidarali <shaidarali915@gmail.com>
2025-08-21 12:14:47 +0500

    resolve the push issue

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     {
       name: "copy-index-to-404",
       closeBundle() {
